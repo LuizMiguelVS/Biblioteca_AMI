16c0e14720e21daa3f35a83c84cd5342d195aaa6
Luiz <miguel342613@gmail.com>
2025-04-03 20:52:30 -0300

    Criando metodo para o usuario pegar o livro emprestado

--- usuario.dart
+++ usuario.dart
@@ -1,5 +1,20 @@
+import 'livro.dart';
+
 class Usuario{
   String nome;
   List<Livro> livrosEmprestados = [];
 
+  Usuario(this.nome);
+
+  void pegarLivroEmprestado(Livro livro){
+
+    if (livro.emprestado == false){
+      livrosEmprestados.add(livro);
+      livro.emprestado = true;
+      print("$nome pegou o livro ${livro.titulo} emprestado");
+    } else {
+      print("O livro ${livro.titulo} se encontra emprestado");
+    }
+
+  }
 }
