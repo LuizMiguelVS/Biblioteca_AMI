42fe597fef47e8c0588e6e21872284b18475b016
Luiz <miguel342613@gmail.com>
2025-04-08 21:49:56 -0300

    Criando método para cadastrar livro

--- biblioteca.dart
+++ biblioteca.dart
@@ -6,6 +6,11 @@ class biblioteca{
   List<Livro> livros = [];
   Map<String, Usuario> usuarios = {};
 
-  
+  void cadastrarLivro (Livro livro){
+
+    livros.add(livro);
+    print("O livro ${livro.titulo} foi cadastrado com sucesso");
+
+  }
 
 }
